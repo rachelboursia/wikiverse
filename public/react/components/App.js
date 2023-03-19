e04e4c8edc7198e074dfc24708901374a71b2cfb
Rachel Boursia <91563448+rachelboursia@users.noreply.github.com>
2023-03-18 21:44:08 -0700

    updates App.js

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -7,12 +7,18 @@ import apiURL from '../api';
 export const App = () => {
 
 	const [pages, setPages] = useState([]);
+	const [selectedArticle, setSelectedArticle] = useState(null);
 
-	async function fetchPages(){
+	async function fetchPages(slug = null){
 		try {
-			const response = await fetch(`${apiURL}/wiki`);
+			const url = slug ? `${apiURL}/wiki/${slug}` : `${apiURL}/wiki`;
+			const response = await fetch(url);
 			const pagesData = await response.json();
+			if (slug) {
+				setSelectedArticle(pagesData);
+			} else {
 			setPages(pagesData);
+			}
 		} catch (err) {
 			console.log("Oh no an error! ", err)
 		}
@@ -22,11 +28,30 @@ export const App = () => {
 		fetchPages();
 	}, []);
 
+	const ArticleDetails = ({article, onBackClick}) => {
+		return (
+			<>
+				<h3>{article.title}</h3>
+				<p>{article.content}</p>
+				<button onClick={onBackClick}>Back to Wiki List</button>
+			</>
+		);
+	}
+
 	return (
 		<main>	
       <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
+		{selectedArticle ? (
+			<ArticleDetails
+				article={selectedArticle}
+				onBackClick={() => setSelectedArticle(null)}
+				/>
+		) : (
+			<PagesList 
+				pages={pages}		
+			 	onArticleClick={(slug) => fetchPages(slug)} 
+			/>
+		)}
 		</main>
 	)
 }
